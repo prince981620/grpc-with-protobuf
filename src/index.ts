69d3ee588013bc8cfabc37708db3357126e12f54
prince981620 <prince981620@gmail.com>
2025-07-15 02:42:13 +0530

    updates index.js to have handlers for better types

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,69 @@
 import path from "path";
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
+import { ProtoGrpcType } from "./generated/a";
+import { AddressBookServiceHandlers } from "./generated/addressbook/AddressBookService";
 
 const PROTO_PATH = path.join(__dirname, "../src/a.proto");
 
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 
-const proto = grpc.loadPackageDefinition(packageDefinition) as any;
+const proto = grpc.loadPackageDefinition(
+  packageDefinition
+) as unknown as ProtoGrpcType;
 const addressBook = proto.addressbook;
 
 const PERSONS: any[] = [];
 
-// Add a person
-// @ts-ignore
-function addPerson(call, callback) {
-  const person = {
-    name: call.request.name,
-    age: call.request.age,
-  };
+const handlers: AddressBookServiceHandlers = {
+  AddPerson(call, callback) {
+    const person = {
+      name: call.request.name,
+      age: call.request.age,
+    };
 
-  PERSONS.push(person);
-  callback(null, person);
-}
+    PERSONS.push(person);
+    callback(null, person);
+  },
 
-// Get all person
-// @ts-ignore
-function getAllPersons(call, callback) {
-  callback(null, { people: PERSONS });
-}
+  GetAllPersons(call, callback) {
+    callback(null, { people: PERSONS });
+  },
+  
+  GetPersonByName(call, callback) {
+    const { name } = call.request;
+    const person = PERSONS.find((p) => p.name === name);
+    callback(null, person || {});
+  },
 
-// Get a person by name
-// @ts-ignore
-function getPersonByName(call, callback) {
-  const { name } = call.request;
-  const person = PERSONS.find((p) => p.name === name);
-  callback(null, person || {});
-}
+  GetPersonsByAge(call, callback) {
+    const { age } = call.request;
+    const matched = PERSONS.filter((p) => p.age === age);
+    callback(null, { people: matched });
+  },
 
-// Get persons by age
-// @ts-ignore
-function getPersonsByAge(call, callback) {
-  const { age } = call.request;
-  const matched = PERSONS.filter((p) => p.age === age);
-  callback(null, { people: matched });
-}
+  DeletePersonByName(call, callback) {
+    const { name } = call.request;
+    const index = PERSONS.findIndex((p) => p.name === name);
 
-// Delete person by name
-// @ts-ignore
-function deletePersonByName(call, callback) {
-  const { name } = call.request;
-  const index = PERSONS.findIndex((p) => p.name === name);
-
-  if (index >= 0) {
-    const [removed] = PERSONS.splice(index, 1);
-    callback(null, removed);
-  } else {
-    callback(null, {});
+    if (index >= 0) {
+      const [removed] = PERSONS.splice(index, 1);
+      callback(null, removed);
+    } else {
+      callback(null, {});
+    }
   }
-}
-
-
+};
 
 // gRPC server
 const server = new grpc.Server();
-server.addService(addressBook.AddressBookService.service, {
-  AddPerson: addPerson,
-  GetAllPersons: getAllPersons,
-  GetPersonByName: getPersonByName,
-  GetPersonsByAge: getPersonsByAge,
-  DeletePersonByName: deletePersonByName,
-});
+server.addService(addressBook.AddressBookService.service, handlers);
 
-server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), () => {
-  console.log("gRPC server running on port 50051");
-  server.start();
-});
+server.bindAsync(
+  "0.0.0.0:50051",
+  grpc.ServerCredentials.createInsecure(),
+  () => {
+    console.log("gRPC server running on port 50051");
+    server.start();
+  }
+);
